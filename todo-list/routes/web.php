35c4a582afddbc3faa5020cf90c329e074d1b686
kavitosik <km220410@gmail.com>
2025-05-20 21:22:13 +0300

    ostro

---
+++ todo-list/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::resource('tasks', TaskController::class);
+Route::get('/', [TaskController::class, 'index']);
