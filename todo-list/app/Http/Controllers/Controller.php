6f360ec08a9d19a04c1f89cb7e729ff157b577eb
kavitosik <km220410@gmail.com>
2025-05-22 21:21:53 +0300

    todo

--- todo-list/app/Http/Controllers/Controller.php
+++ todo-list/app/Http/Controllers/Controller.php
@@ -5,4 +5,4 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
-}
+ }
--- todo-list/routes/web.php
+++ todo-list/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+use App\Http\Controllers\TaskController;
 
 Route::resource('tasks', TaskController::class);
 Route::get('/', [TaskController::class, 'index']);
